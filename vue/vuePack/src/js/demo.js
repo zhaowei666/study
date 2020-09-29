7773c40b3ab9e9878052eda1496928063dc1a01d
baixing <zw204801@163.com>
2020-09-29 18:44:47 +0800

    webpack config

--- vue/vuePack/src/js/demo.js
+++ vue/vuePack/src/js/demo.js
@@ -1,6 +1,9 @@
 import $ from 'jquery'
+import '../css/index.css'
+import '../css/test.less'
+import '../css/test1.scss'
 
 $(function() {
-    $('li:odd').css('backgroundColor', 'blue')
-    $('li:even').css('backgroundColor', 'lightblue')
+    $('li:odd').css('backgroundColor', '#ccc')
+    $('li:even').css('backgroundColor', 'skyblue')
 })
--- vue/vuePack/webpack.config.js
+++ vue/vuePack/webpack.config.js
@@ -1,10 +1,38 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const htmlPlugin = new HtmlWebpackPlugin({
+    template: './src/index.html',
+    filename: 'index.html'
+})
 module.exports = {
     mode: "development",
     entry: path.join(__dirname, './src/js/demo.js'),
-    output:{
-        path:path.join(__dirname, './dist'),
-        filename:'bundle.js'
+    output: {
+        path: path.join(__dirname, './dist'),
+        filename: 'bundle.js',
+    },
+    plugins: [htmlPlugin],
+    module: {
+        rules: [
+            {
+                //test设置需要匹配的文件类型，支持正则
+                test:/\.css$/,
+                //use表示该文件类型需要调用的loader
+                use:['style-loader','css-loader']
+            },
+            {
+                test:/\.less$/,
+                use:['style-loader','css-loader','less-loader']
+            },
+            {
+                test:/\.scss$/,
+                use:['style-loader','css-loader','sass-loader']
+            },{
+                test:/\.jpg|png|gif|bmp|ttf|eot|svg|woff|woff2$/,
+                //limit用来设置字节数，只有小于limit值的图片，才会转换
+                //为base64图片
+                use:"url-loader?limit=16940"
+            }
+        ]
     }
-
 }
